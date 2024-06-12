9c0603e91f6e573962dd56338b4243e18ea6ecac
italy <a_iacevedo@colegiosanlorenzo.org>
2024-06-12 14:15:29 -0400

    mi primer commit

---
+++ app/__init__.py
@@ -0,0 +1,4 @@
+"""Configuracion de la aplicación."""
+
+# flask
+from flask import Flask
---
+++ server.py
@@ -0,0 +1,7 @@
+"""server app."""
+
+from app import app
+
+#ejecutar la alicación
+if __name__=="__main__":
+    app.rum(debug=true)
